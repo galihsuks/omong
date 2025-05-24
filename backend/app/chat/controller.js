51d3b32e46ca8e9130e2aab83732f9bed286b62f
galihsuks <galih8.4.2001@gmail.com>
2025-05-24 22:26:10 +0700

    tambah webscoekt dan hapus env frontned

--- backend/app/chat/controller.js
+++ backend/app/chat/controller.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const Chat = require("./model.js");
 const Room = require("../room/model.js");
+const WebSocket = require("ws");
 
 const addChat = async (req, res) => {
     try {
@@ -23,7 +25,26 @@ const addChat = async (req, res) => {
         };
         const chat = new Chat(data);
         await chat.save();
-        res.status(200).json(chat);
+
+        const ws = new WebSocket(process.env.URL_WS);
+        ws.onopen = () => {
+            ws.send(
+                JSON.stringify({
+                    tipe: "chat",
+                    data: {
+                        ...chat._doc,
+                        action: "add",
+                    },
+                }),
+                (err) => {
+                    res.status(200).json(chat);
+                    ws.close();
+                }
+            );
+        };
+        ws.onerror = (err) => {
+            res.status(400).json({ pesan: "WebSocket error: " + err.message });
+        };
     } catch (error) {
         res.status(500).json({ pesan: error.message });
     }
@@ -47,7 +68,26 @@ const delChat = async (req, res) => {
                 .json({ pesan: "Anda tidak memiliki akses ke room ini" });
         }
         await Chat.findByIdAndDelete(req.params.id);
-        res.status(200).json(chat);
+
+        const ws = new WebSocket(process.env.URL_WS);
+        ws.onopen = () => {
+            ws.send(
+                JSON.stringify({
+                    tipe: "chat",
+                    data: {
+                        ...chat._doc,
+                        action: "delete",
+                    },
+                }),
+                (err) => {
+                    res.status(200).json(chat);
+                    ws.close();
+                }
+            );
+        };
+        ws.onerror = (err) => {
+            res.status(400).json({ pesan: "WebSocket error: " + err.message });
+        };
     } catch (error) {
         res.status(500).json({ pesan: error.message });
     }
